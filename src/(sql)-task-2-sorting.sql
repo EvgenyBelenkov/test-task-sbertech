1142ecf87fb7cfa5819bce83d407f844f9f8063f
jehbk <eu.belenkov@yandex.ru>
2020-02-26 20:34:14 +0300

    commit

--- src/(sql)-task-2-sorting.sql
+++ src/(sql)-task-2-sorting.sql
@@ -1,6 +1,11 @@
 SELECT lpad(' ', 3*level)||name as TreeTable
-FROM test_table
-WHERE name LIKE '%a%'
-START WITH ParentID = 0
-CONNECT BY PRIOR id = ParentID
-ORDER SIBLINGS BY ParentID;
+  FROM (
+    SELECT distinct *
+      FROM test_table t
+     START WITH name LIKE '%a%'
+       and not exists(select 1 from test_table x where x.parentId = t.id)
+   CONNECT BY id = PRIOR ParentID
+  ) x
+   START WITH ParentID = 0
+ CONNECT BY PRIOR id = ParentID
+ ORDER SIBLINGS BY ParentId;
